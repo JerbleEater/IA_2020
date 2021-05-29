bd22409f71040d3a85970ce472fbe2f152a34373
jamacas10 <83563154+JerbleEater@users.noreply.github.com>
2021-05-28 22:43:09 -0600

    Major Unity Engine Update: Updated the from LTS 2019.4.1f to 2019.4.27f. Updated 2D animation, Post Processing and TextMeshPro Packages. Other Changes: Changed folder names

---
+++ IA_UD/Assets/Scripts/Enemy.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    // Public Vars
+    public EnemySpriteDatabase enemySprites;
+
+    // Private Vars
+    private SwipeGesture.Direction currDirection;
+    private Animator animArrow;
+    private Slider hpSlider;
+    private string animToPlay;
+    private float hp, movementSpeed;
+
+    private void Awake(){
+        // Assign hp and attach enemySpritePrefab to the canvas of the new enemy prefab
+        hp            = enemySprites.randomEnemySelector(this.transform.GetChild(0));
+        movementSpeed = 50f;
+        animArrow     = gameObject.GetComponentInChildren<Animator>();
+        hpSlider      = gameObject.GetComponentInChildren<Slider>();
+        
+        hpSlider.maxValue = hp;
+        hpSlider.value    = hp;
+    }
+
+    private void Start(){
+        generateDirection();
+    }
+
+    private void FixedUpdate() { 
+        // Ensures animator is displaying correct arrow direction
+        if(!animArrow.GetCurrentAnimatorStateInfo(0).IsName(animToPlay)){
+            animArrow.Play(animToPlay);
+        }
+        // Moves enemy forward
+        this.transform.position += -Vector3.forward * Time.deltaTime * movementSpeed;
+    }
+
+    /*
+        Function: On Trigger Enter, Params: Collider
+        This function checks if the box collider overlaps with the KillBox collider
+        a call is made to the GameHandler and the enemy obj is destroyed. 
+    */
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "KillBox"){
+            GameHandler.Instance.fuckPlayerOneUp();
+            Destroy(gameObject);
+        }    
+    }
+
+    /*
+        Function: Generate Direction, Params: None
+        This function uses a switch statement to determine which arrow direction
+        to assign the enemy, along with which direciton animation to begin playing.  
+    */
+    private void generateDirection(){
+
+        int rand = Random.Range(0, 4);
+
+        switch(rand){
+            case 0:
+                currDirection = SwipeGesture.Direction.Left;
+                animToPlay = "left";
+                break;
+            case 1:
+                currDirection = SwipeGesture.Direction.Up;
+                animToPlay = "up";
+                break;
+            case 2:
+                currDirection = SwipeGesture.Direction.Right;
+                animToPlay = "right";
+                break;
+            case 3:
+                currDirection = SwipeGesture.Direction.Down;
+                animToPlay = "down";
+                break;
+        }
+        animArrow.Play(animToPlay);
+    }
+
+    /*
+        Function: Decrease Hp, Params: Float
+        This function subtracts the amount of damage provided from
+        the current hp. Visually changes the hp slider value as well.
+        If current hp is less than 0, the enemy obj is destroyed.   
+    */
+    private void decreaseHp(float damage){
+        hp -= damage;
+        hpSlider.value = hp;
+        
+        if(hp <= 0.0f){
+            Destroy(this.gameObject);
+        }
+    }
+
+    /*
+        Function: Check Current Direction, Params: SwipeGesture.Direction, Float
+        This function checks if the player direction matches the enemies arrow direction.
+        If matched, enemy hp is decreased based on players total damage. A new arrow direction
+        is generated.  
+    */
+    public void checkCurrentDirection(SwipeGesture.Direction playerDirection, float damage){
+        if(playerDirection == currDirection){
+            decreaseHp(damage);  
+            // Ensures repeat direction are played from the beginning.
+            animArrow.Play("start");      
+            generateDirection();
+        }
+    }
+
+}
---
+++ IA_UD/Assets/Scripts/Item.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Item : MonoBehaviour{   
+    // Public Vars
+    public enum ItemType{Heart, Grenade, Nuke, AutoTurrent, GoldenHearts, Damage, CritChance, InstaKillMF, MoMoney};
+    public enum CurrentState{OnFloor, InInventory};
+    
+    // Private Vars
+    private ItemType itemType;
+    private CurrentState itemState;
+    private float damageAdded, critChanceAdded, lerpTime;
+    private bool followMousePos;
+    private Player player;
+
+    private void Awake(){
+        /* I should rethink about doing this, the item should be added to the inventory of
+        the player who clicked on the item. This only works in a single player game
+        not multiplayer game, as there would be more than one main player. 
+        */
+        player = GameObject.Find("MainPlayer").GetComponent<Player>();
+    }
+
+    private void FixedUpdate(){
+        if(followMousePos){
+            var mousePos = Input.mousePosition;
+            mousePos.z = 1f;
+            this.GetComponent<RectTransform>().position = mousePos;
+        } 
+    }
+
+    /*
+        Function: Item Action, Params: None
+        This function uses a switch statement to determine what the item should do
+        based on the items type. 
+    */
+    private void itemAction(){
+        switch(itemType){
+            case ItemType.Heart:
+                player.increaseInGameHp();
+                break;
+            case ItemType.Grenade:
+                break;
+            default:
+                break;
+        }
+    }
+
+    /*
+        Function: Item Clicked(Event), Params: None
+        This function is triggered when an item is clicked. It checks the state of the 
+        item(OnFloor || InInventory). A switch statement is used to determine if the item 
+        should be added to the player inventory or removed. In the instance that the item 
+        is removed, the followMousePos bool is checked on.
+    */
+    private void itemClicked(){
+        switch(itemState){
+            case CurrentState.OnFloor:
+                player.addItemToInventory(this.gameObject, this.itemType);
+                break;
+            case CurrentState.InInventory:
+                player.removeItemFromInventory(this.gameObject, this.itemType);
+                followMousePos = true;
+                break;
+        }
+    }
+
+    /*
+        Function: Item Unclicked(Event), Params: None
+        This function is triggered when an item is unclicked. It sets the followMousePos off. 
+        If the pos of the item when unclicked is below the specified y pos, it is added back to 
+        the player inventory, otherwise the items action is executed and the item is destroyed.
+        Items are set to InInventory state if they are not destroyed.
+    */
+    private void itemUnclicked(){
+        followMousePos = false;
+
+        RectTransform itemRT = this.gameObject.GetComponent<RectTransform>();
+
+        if(itemRT.localPosition.y < -450f){
+            player.addItemToInventory(this.gameObject, this.itemType);
+        } else if(itemState == CurrentState.InInventory){
+            itemAction();
+            Destroy(this.gameObject);
+        }
+
+        itemState = CurrentState.InInventory;
+
+    }
+
+    // Setters
+    public void setItemState(CurrentState itemState){
+        this.itemState = itemState;
+    }
+
+    // Getters
+    public float getItemDamageAdded(){
+        return damageAdded;
+    }
+
+    public float getItemCritChanceAdded(){
+        return critChanceAdded;
+    }
+
+}
